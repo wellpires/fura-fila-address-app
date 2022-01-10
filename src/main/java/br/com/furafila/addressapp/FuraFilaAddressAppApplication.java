726be3ad96bb78e4e3685de8782f3f33d6701c52
Wellington PIres <wellington.goncalves@live.com>
2022-01-10 19:37:06 -0300

    First commit

---
+++ src/main/java/br/com/furafila/addressapp/FuraFilaAddressAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.furafila.addressapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuraFilaAddressAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuraFilaAddressAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/furafila/addressapp/FuraFilaAddressAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.furafila.addressapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuraFilaAddressAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
