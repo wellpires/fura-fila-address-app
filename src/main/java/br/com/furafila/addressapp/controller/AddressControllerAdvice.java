29ce92d03ca7bf7797987a381ad92b52ca0ea731
Wellington Pires <wellington.goncalves@live.com>
2022-06-17 23:33:26 +0100

    Adding logger to Serve ErrorApiException

--- src/main/java/br/com/furafila/addressapp/controller/AddressControllerAdvice.java
+++ src/main/java/br/com/furafila/addressapp/controller/AddressControllerAdvice.java
@@ -5,11 +5,9 @@ import java.util.Objects;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import br.com.furafila.addressapp.exception.AddressTypeNotFoundException;
--- src/main/java/br/com/furafila/addressapp/exception/ServerErrorApiException.java
+++ src/main/java/br/com/furafila/addressapp/exception/ServerErrorApiException.java
@@ -1,13 +1,18 @@
 package br.com.furafila.addressapp.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 
 public class ServerErrorApiException extends RuntimeException {
+
+	private static final Logger logger = LoggerFactory.getLogger(ServerErrorApiException.class);
 
 	private static final long serialVersionUID = -361826878162465768L;
 
 	public ServerErrorApiException(String path, HttpStatus statusCode) {
 		super(String.format("%s - %s - [%s]", statusCode.value(), statusCode.getReasonPhrase(), path));
+		logger.error(this.getMessage());
 	}
 
 }
